7958742aebd172c005afe86915b5975018f2371b
Tharunvasireddi <tharunvasireddy73@gmail.com>
2025-12-10 07:39:49 +0530

    feat: enhance flashcard controller with fetching and reviewing functionalities

--- backend/controllers/flashcardController.js
+++ backend/controllers/flashcardController.js
@@ -1,17 +1,70 @@
 import { FlashCard } from "../models/FlashCard.js";
 
+// get all flashcards of the user
+
 const getAllFlashCardController = async (req, res) => {
   try {
-  } catch (error) {}
+    const allFlashCards = await FlashCard.find({
+      UserId: req.user._id,
+    })
+      .populate("documentId", "title")
+      .sort({ createdAt: -1 });
+    res.status(200).json({
+      success: true,
+      message: "all flash cards are feched successfully",
+      data: allFlashCards,
+      count: allFlashCards.length,
+    });
+  } catch (error) {
+    console.log("error while fetching the flashcards :", error);
+  }
 };
 
 const getFlashCards = async (req, res) => {
   try {
-  } catch (error) {}
+    const flashCards = await flashCards
+      .find({ UserId: req.user._id, documentId: req.params.documentId })
+      .populate("document", "title filename")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({
+      success: true,
+      message: "flash card is fetched successfully",
+      data: flashCards,
+      count: flashCards.length,
+    });
+  } catch (error) {
+    console.log("error while fectching flash card", error);
+  }
 };
 
 const reviewFlashCard = async (req, res) => {
   try {
+    const flashCardSet = await FlashCard.findOne({
+      "cards._id": req.params._id,
+      userId: req.user._id,
+    });
+
+    const flashCardIndex = flashCardSet.cards.findIndex(
+      (card) => card._id.toString() === req.params._id
+    );
+    if (flashCardIndex === -1) {
+      res.status(404).json({
+        success: true,
+        message: "flash card is not found",
+      });
+    }
+    const flashCard = flashCardSet.cards[flashCardIndex];
+    flashCard.lastReviewed = Date.now();
+    flashCard.reviewCount = flashCard.reviewCount + 1;
+
+
+    await flashCardSet.save();
+    res.status(200).json({
+      success: true,
+      message: "flash card is reviewed successfully",
+      data : flashCardSet
+    });
   } catch (error) {}
 };
 
